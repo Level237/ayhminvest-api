cf608efc892933f30280b9dd5c2a63fd7b3806e0
level237 <Bramslevel129@gmail.com>
2024-12-14 18:32:06 +0100

    update payment

--- app/Http/Controllers/Api/Payment/StripeController.php
+++ app/Http/Controllers/Api/Payment/StripeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Api\Payment;
 
+
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Stripe\Stripe;
 use Stripe\Charge;
 class StripeController extends Controller
@@ -19,7 +22,10 @@ class StripeController extends Controller
                 'source' => $request->stripeToken,
                 'description' => 'Investissement',
             ]);
-
+            $payment=new Payment;
+            $payment->user_id=Auth::guard("api")->user()->id;
+            $payment->price=$request->amount;
+            $payment->save();
             return response()->json($charge);
         }catch(\Exception $e){
             return response()->json([
